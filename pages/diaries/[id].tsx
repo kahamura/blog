136bda21145eac7cc7b925c893e8a3e7a21876bf
ka-aki <aki0215ka@gmail.com>
2022-04-02 14:03:01 +0900

    fix stye

--- pages/diaries/[id].tsx
+++ pages/diaries/[id].tsx
@@ -38,7 +38,7 @@ const Diary: React.FC<Props> = ({ diaryData }) => {
       <div className="text-4xl dark:text-white font-bold mb-3">
         {diaryData.title}
       </div>
-      <div className="pb-10">
+      <div className="pb-4">
         <Date dateString={diaryData.date} />
       </div>
       <div dangerouslySetInnerHTML={{ __html: diaryData.contentHtml ?? "" }} />
